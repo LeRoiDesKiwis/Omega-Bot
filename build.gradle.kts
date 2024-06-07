89bfee1542a2c99655c0051ec5f9efbdf0c84b34
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-06-07 16:17:37 +0200

    Add JDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.dv8tion:JDA:5.0.0-beta.24")
 }
 
 tasks.test {
