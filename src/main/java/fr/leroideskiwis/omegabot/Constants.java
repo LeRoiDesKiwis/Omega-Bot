db191222625796b88048b05e923b9de35795d5dd
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-06-09 13:40:32 +0200

    Add logs

---
+++ src/main/java/fr/leroideskiwis/omegabot/Constants.java
@@ -0,0 +1,7 @@
+package fr.leroideskiwis.omegabot;
+
+public class Constants {
+
+    public static long logChannel = 1249322994187374734L;
+
+}
--- src/main/java/fr/leroideskiwis/omegabot/command/CommandManager.java
+++ src/main/java/fr/leroideskiwis/omegabot/command/CommandManager.java
@@ -1,12 +1,17 @@
 package fr.leroideskiwis.omegabot.command;
 
+import fr.leroideskiwis.omegabot.Constants;
 import fr.leroideskiwis.omegabot.events.EventManager;
 import fr.leroideskiwis.omegabot.user.OmegaUser;
 import fr.leroideskiwis.omegabot.user.UserManager;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,5 +59,17 @@ public class CommandManager {
                 .filter(command -> command.getKey().equalsIgnoreCase(event.getName()))
                 .findFirst()
                 .ifPresent(command -> command.getValue().execute(user, event));
+
+        event.getGuild().getChannelById(TextChannel.class, Constants.logChannel).sendMessageEmbeds(createLogEmbed(event)).queue();
+    }
+
+    private MessageEmbed createLogEmbed(SlashCommandInteraction event){
+
+        EmbedBuilder embedBuilder = new EmbedBuilder()
+                .setColor(Color.YELLOW)
+                .setAuthor("Commande executee par LeRoiDesKiwis", null, event.getMember().getAvatarUrl())
+                .setDescription(event.getCommandString())
+                .setTimestamp(event.getTimeCreated());
+        return embedBuilder.build();
     }
 }
