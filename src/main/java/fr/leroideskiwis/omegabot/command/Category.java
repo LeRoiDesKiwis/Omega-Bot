01278c4f0278cfb6aaf61cfdcc5d31cf2af1e5ed
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-06-11 01:17:36 +0200

    Change sanctions emote

--- src/main/java/fr/leroideskiwis/omegabot/command/Category.java
+++ src/main/java/fr/leroideskiwis/omegabot/command/Category.java
@@ -1,7 +1,7 @@
 package fr.leroideskiwis.omegabot.command;
 
 public enum Category {
-    BOUTIQUE_CANAUX(":speaker:"), BOUTIQUE_SANCTIONS(":bomb:"), BOUTIQUE_FUN(":tada:"), BANQUE(":moneybag:"), ADMIN(":no_entry_sign:"), DIVERS(":flushed:");
+    BOUTIQUE_CANAUX(":speaker:"), BOUTIQUE_SANCTIONS(":smiling_imp:"), BOUTIQUE_FUN(":tada:"), BANQUE(":moneybag:"), ADMIN(":no_entry_sign:"), DIVERS(":flushed:");
 
     String emote;
 
