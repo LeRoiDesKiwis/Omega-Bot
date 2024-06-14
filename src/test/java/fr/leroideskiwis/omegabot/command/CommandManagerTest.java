1454cafb3d74870f3f565be34741dcbd41b3c25e
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-06-14 13:03:35 +0200

    Add CommandManagerTest and MessageListenerTest

---
+++ src/test/java/fr/leroideskiwis/omegabot/command/CommandManagerTest.java
@@ -0,0 +1,77 @@
+package fr.leroideskiwis.omegabot.command;
+
+import fr.leroideskiwis.omegabot.user.OmegaUser;
+import fr.leroideskiwis.omegabot.user.UserManager;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.requests.RestAction;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class CommandManagerTest {
+
+    private CommandManager commandManager;
+    private Command command;
+    private SlashCommandInteraction slashCommandInteraction;
+    private OmegaUser user;
+    private UserManager userManager;
+    private JDA jda;
+
+    @BeforeEach
+    void setUp() {
+        userManager = mock(UserManager.class);
+        user = mock(OmegaUser.class);
+        when(userManager.from(any())).thenReturn(user);
+
+        jda = mock(JDA.class);
+        RestAction restAction = mock(RestAction.class);
+        when(jda.upsertCommand(any())).thenReturn(restAction);
+        doNothing().when(restAction).queue();
+
+        command = mock(Command.class);
+        SlashCommandData commandData = mock(SlashCommandData.class);
+        when(command.commandData()).thenReturn(commandData);
+        when(commandData.getName()).thenReturn("test");
+        when(command.category()).thenReturn(Category.BOUTIQUE_FUN);
+
+        slashCommandInteraction = mock(SlashCommandInteraction.class);
+        commandManager = new CommandManager(jda, userManager, command);
+    }
+
+    @Test
+    void execute() {
+        when(slashCommandInteraction.getName()).thenReturn("test");
+        commandManager.execute(slashCommandInteraction);
+        verify(command).execute(user, slashCommandInteraction);
+    }
+
+    @Test
+    void dontexecute() {
+        when(slashCommandInteraction.getName()).thenReturn("test1");
+        commandManager.execute(slashCommandInteraction);
+        verify(command, never()).execute(user, slashCommandInteraction);
+    }
+
+    @Test
+    void forEach() {
+        Map<String, Class<? extends Command>> expected = Map.of("help", HelpCommand.class, "test", command.getClass());
+        commandManager.forEach((s, command) -> assertEquals(expected.get(s), command.getClass()));
+    }
+
+    @Test
+    void getByCategory() {
+        assertEquals(0, commandManager.getByCategory(Category.BANQUE).size());
+        assertEquals(1, commandManager.getByCategory(Category.DIVERS).size());
+        assertEquals(0, commandManager.getByCategory(Category.ADMIN).size());
+        assertEquals(0, commandManager.getByCategory(Category.BOUTIQUE_CANAUX).size());
+        assertEquals(0, commandManager.getByCategory(Category.BOUTIQUE_SANCTIONS).size());
+        assertEquals(1, commandManager.getByCategory(Category.BOUTIQUE_FUN).size());
+    }
+}
---
+++ src/test/java/fr/leroideskiwis/omegabot/listeners/MessageListenerTest.java
@@ -0,0 +1,48 @@
+package fr.leroideskiwis.omegabot.listeners;
+
+import fr.leroideskiwis.omegabot.events.EventManager;
+import fr.leroideskiwis.omegabot.user.OmegaUser;
+import fr.leroideskiwis.omegabot.user.UserManager;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class MessageListenerTest {
+
+    private MessageListener messageListener;
+    private MessageReceivedEvent event;
+    private OmegaUser user;
+    private EventManager eventManager;
+    private UserManager userManager;
+    private User realUser;
+
+    @BeforeEach
+    void setUp() {
+        user = mock(OmegaUser.class);
+        realUser = mock(User.class);
+        when(realUser.isBot()).thenReturn(false);
+        event = mock(MessageReceivedEvent.class);
+        when(event.getAuthor()).thenReturn(realUser);
+
+        eventManager = mock(EventManager.class);
+        doNothing().when(eventManager).handle(event);
+
+        userManager = mock(UserManager.class);
+        when(userManager.from(any())).thenReturn(user);
+
+        messageListener = new MessageListener(eventManager, userManager);
+    }
+
+    @Test
+    void onMessageReceived() {
+        messageListener.onMessageReceived(event);
+        messageListener.onMessageReceived(event);
+        verify(user, times(1)).givePoints(1);
+
+    }
+}
