eccc787e70b9db6a3b2e44042be60b9c5841efa2
apoleon33 <simondumesny@gmail.com>
2024-06-16 19:08:25 +0200

    Optionnal argument "version" to choose which changelog to read.

--- src/main/java/fr/leroideskiwis/omegabot/command/other/ChangelogCommand.java
+++ src/main/java/fr/leroideskiwis/omegabot/command/other/ChangelogCommand.java
@@ -4,7 +4,10 @@ import fr.leroideskiwis.omegabot.Main;
 import fr.leroideskiwis.omegabot.command.Category;
 import fr.leroideskiwis.omegabot.command.Command;
 import fr.leroideskiwis.omegabot.user.OmegaUser;
+import fr.leroideskiwis.omegabot.user.UserManager;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
@@ -13,19 +16,23 @@ import java.awt.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 
 public class ChangelogCommand implements Command {
 
     @Override
     public SlashCommandData commandData() {
-        return Commands.slash("changelog", "Dernières nouveautés du Bot.");
+        return Commands.slash("changelog", "Dernières nouveautés du Bot.").addOption(OptionType.STRING, "version", "Le numéro de la version (de la forme X.Y.Z)", false);
     }
 
     @Override
     public void execute(OmegaUser user, SlashCommandInteraction event) {
-        EmbedBuilder builder = new EmbedBuilder().setTitle("Nouveautés de la version v" + Main.version).setColor(new Color(109, 50, 109));
+        OptionMapping userInput = event.getOption("version");
+        String changelogNumber = (userInput == null) ? Main.version : userInput.getAsString();
 
-        String changelogFilePath = "changelog/" + Main.version + ".md";
+        EmbedBuilder builder = new EmbedBuilder().setTitle("Nouveautés de la version v" + changelogNumber).setColor(new Color(109, 50, 109));
+
+        String changelogFilePath = "changelog/" + changelogNumber + ".md";
 
         try {
             List<String> changelog = Files.readAllLines(Path.of(changelogFilePath));
@@ -33,7 +40,7 @@ public class ChangelogCommand implements Command {
 
         } catch (Exception e) {
             builder.setColor(Color.red);
-            builder.setDescription("Pas de changelog pour la v" + Main.version + " trouvé");
+            builder.setDescription("Pas de changelog pour la v" + changelogNumber + " trouvé");
         }
 
         event.replyEmbeds(builder.build()).setEphemeral(true).queue();
