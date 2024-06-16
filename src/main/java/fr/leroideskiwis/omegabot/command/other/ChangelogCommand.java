9287216e669e1eb4bcdb735167f6c0636777d854
apoleon33 <simondumesny@gmail.com>
2024-06-16 19:28:20 +0200

    Add check if the version option is a valid version number

--- src/main/java/fr/leroideskiwis/omegabot/command/other/ChangelogCommand.java
+++ src/main/java/fr/leroideskiwis/omegabot/command/other/ChangelogCommand.java
@@ -4,7 +4,6 @@ import fr.leroideskiwis.omegabot.Main;
 import fr.leroideskiwis.omegabot.command.Category;
 import fr.leroideskiwis.omegabot.command.Command;
 import fr.leroideskiwis.omegabot.user.OmegaUser;
-import fr.leroideskiwis.omegabot.user.UserManager;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -16,19 +15,28 @@ import java.awt.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class ChangelogCommand implements Command {
 
     @Override
     public SlashCommandData commandData() {
-        return Commands.slash("changelog", "Dernières nouveautés du Bot.").addOption(OptionType.STRING, "version", "Le numéro de la version (de la forme X.Y.Z)", false);
+        return Commands.slash("changelog", "Dernières nouveautés du Bot.").addOption(OptionType.STRING, "version", "Le numéro de version (de la forme X.Y.Z)", false);
     }
 
     @Override
     public void execute(OmegaUser user, SlashCommandInteraction event) {
         OptionMapping userInput = event.getOption("version");
         String changelogNumber = (userInput == null) ? Main.version : userInput.getAsString();
+
+        // check if
+        boolean isValidChangelog = Pattern.compile("^(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)$").matcher(changelogNumber).find();
+        if (!isValidChangelog){
+            EmbedBuilder errorBuilder = new EmbedBuilder().setTitle("Numéro de version invalide").setColor(Color.red);
+            errorBuilder.addField(changelogNumber + " n'est pas un numéro de version valide", "Le numéro de version doit être de la forme X.Y.Z (ex: " + Main.version + ")", false);
+            event.replyEmbeds(errorBuilder.build()).setEphemeral(true).queue();
+            return;
+        }
 
         EmbedBuilder builder = new EmbedBuilder().setTitle("Nouveautés de la version v" + changelogNumber).setColor(new Color(109, 50, 109));
 
