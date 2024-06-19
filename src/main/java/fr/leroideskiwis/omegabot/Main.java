ac92ffbea22ee8542795b2f0df78892a8e141736
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-06-19 02:13:48 +0200

    1.4.4 -> 1.5

--- src/main/java/fr/leroideskiwis/omegabot/Main.java
+++ src/main/java/fr/leroideskiwis/omegabot/Main.java
@@ -24,7 +24,7 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
 
-    public static String version = "1.4.4";
+    public static String version = "1.5";
     private CommandManager commandManager;
     private EventManager eventManager;
     private UserManager userManager;
