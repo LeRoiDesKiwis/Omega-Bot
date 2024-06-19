3978403c5f240341cfc264a0042effb73868eb2c
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-06-19 23:08:12 +0200

    feat(classement): add option user

--- src/main/java/fr/leroideskiwis/omegabot/command/fun/ClassementCommand.java
+++ src/main/java/fr/leroideskiwis/omegabot/command/fun/ClassementCommand.java
@@ -5,6 +5,8 @@ import fr.leroideskiwis.omegabot.command.Command;
 import fr.leroideskiwis.omegabot.user.OmegaUser;
 import fr.leroideskiwis.omegabot.user.UserManager;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
@@ -22,18 +24,22 @@ public class ClassementCommand implements Command {
 
     @Override
     public SlashCommandData commandData() {
-        return Commands.slash("classement", "Affiche le classement des membres");
+        return Commands.slash("classement", "Affiche le classement des membres")
+                .addOption(OptionType.USER, "user", "Utilisateur", false);
     }
 
     @Override
     public void execute(OmegaUser user, SlashCommandInteraction event) {
+
+        Member member = event.getOption("user") == null ? event.getMember() : event.getOption("user").getAsMember();
+
         StringBuilder builder = new StringBuilder();
         AtomicInteger counter = new AtomicInteger(1);
         userManager.stream()
                 .sorted((o1, o2) -> o2.getPoints() - o1.getPoints())
                 .forEach(omegaUser -> {
                     int i = counter.getAndIncrement();
-                    if(i <= 10 || omegaUser.isMember(event.getMember())){
+                    if(i <= 10 || omegaUser.isMember(member)){
                         builder.append(i)
                                 .append(" - ")
                                 .append(omegaUser.getName())
