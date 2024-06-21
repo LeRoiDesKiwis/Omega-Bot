607f830e284171f83a88938a06c19d9ec5f88d0e
LeRoiDesKiwis <lekiwidu17@gmail.com>
2024-06-22 01:36:22 +0200

    1.6.1 -> 1.7

--- src/main/java/fr/leroideskiwis/omegabot/Main.java
+++ src/main/java/fr/leroideskiwis/omegabot/Main.java
@@ -31,7 +31,7 @@ import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Main extends ListenerAdapter {
 
-    public static String version = "1.6.1";
+    public static String version = "1.7";
     private CommandManager commandManager;
     private EventManager eventManager;
     private UserManager userManager;
